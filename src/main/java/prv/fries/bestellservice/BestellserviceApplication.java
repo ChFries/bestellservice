3fee2fc1fe72914510f71e8c0db6a7b82d0891c8
Christopher Fries <ch.fries@gmx.net>
2025-07-09 11:03:52 +0200

    feat(#7): Initial commit

---
+++ src/main/java/prv/fries/bestellservice/BestellserviceApplication.java
@@ -0,0 +1,13 @@
+package prv.fries.bestellservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestellserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestellserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prv/fries/bestellservice/BestellserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package prv.fries.bestellservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestellserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
