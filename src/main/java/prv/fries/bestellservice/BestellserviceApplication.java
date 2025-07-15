c8235b0fc5eb051e188c15cb33b8406f321b8d07
chris <ch.fries@gmx.net>
2025-07-15 20:03:23 +0200

    feat(#13): Implement Client Generation for payment.yaml

--- src/main/java/prv/fries/bestellservice/BestellserviceApplication.java
+++ src/main/java/prv/fries/bestellservice/BestellserviceApplication.java
@@ -2,7 +2,9 @@ package prv.fries.bestellservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -13,4 +15,8 @@ public class BestellserviceApplication {
 		SpringApplication.run(BestellserviceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
---
+++ src/main/java/prv/fries/bestellservice/bestellung/client/payment/PaymentConfiguration.java
@@ -0,0 +1,29 @@
+package prv.fries.bestellservice.bestellung.client.payment;
+
+import org.openapitools.client.api.ZahlungApi;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import prv.fries.bestellservice.generated.client.ApiClient;
+
+@Configuration
+public class PaymentConfiguration {
+
+
+    @Value("${payment.url}")
+    private String paymentUrl;
+
+    @Bean("paymentApiClient")
+    public ApiClient apiClient(RestTemplate restTemplate) {
+        ApiClient apiClient = new ApiClient(restTemplate);
+        apiClient.setBasePath(paymentUrl);
+        return apiClient;
+    }
+
+    @Bean
+    public ZahlungApi zahlungApi(@Qualifier("paymentApiClient") ApiClient apiClient) {
+        return new ZahlungApi(apiClient);
+    }
+}
